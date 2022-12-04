53ee2993b244fe249e07b4c146d5eada2d9acd92
Jeffrey Zhen <jeffreyzhen7890@gmail.com>
2022-12-04 11:05:43 -0500

    add more machines

--- main.tf
+++ main.tf
@@ -1,26 +1,34 @@
 terraform {
-    required_providers {
-        proxmox = {
-            source  = "telmate/proxmox"
-            version = "2.9.11"
-        }
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.11"
     }
+  }
 }
 
 provider "proxmox" {
-    # Configuration options
-    pm_api_url = var.pm_api_url
+  # Configuration options
+  pm_api_url = var.pm_api_url
 }
 
 resource "proxmox_vm_qemu" "vm1" {
-    name        = "VM1"
-    target_node = var.target_node
+  name        = "VM1"
+  count       = 1
+  target_node = var.target_node
 
-    #iso        = "ISO file name"
+  # Clone VM from template
+  clone = "ubuntu22.04"
 
-    # Clone VM from template
-    clone = "ubuntu22.04"
+  # Other params
+  memory = 2048
+  cores = 2
+	agent  = 1
+}
 
-    # Other params
-    memory = 2048
+output "vm_ip_addresses" {
+  value = {
+    for vm in proxmox_vm_qemu.vm1 :
+    vm.name => vm.ssh_host
+  }
 }
