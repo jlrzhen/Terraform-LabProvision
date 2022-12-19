0b77fce992541fc76401c1bf1529a68a0e46d697
jlrzhen <jeffreyzhen7890@gmail.com>
2022-12-19 03:24:58 +0000

    update scripts

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,1 +1,27 @@
-git update-index --skip-worktree .env
+setup() {
+    git update-index --skip-worktree .env
+    echo "setup complete"
+}
+
+teardown() {
+    git update-index --no-skip-worktree .env
+    echo "teardown complete"
+}
+
+PS3="Select action: "
+select option in Setup Teardown Exit
+do
+    case $option in
+        "Setup")
+            setup
+            break;;
+        "Teardown")
+            teardown
+            break;;
+        "Exit")
+            echo "exit"
+            break;;
+        *)
+            echo "Please select an action.";;
+    esac
+done
